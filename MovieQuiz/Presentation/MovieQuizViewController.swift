e36dc4ed67c7a3980ac2b00413be500f441b6c19
Yung Lord <marikoryagina.den@yandex.ru>
2025-10-16 16:51:59 +0300

    Закончил верстку основного экрана

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -7,7 +7,6 @@ final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelViewFont.font = UIFont(name: "YSDisplay-Bold", size: 23)
     }
 }
 
